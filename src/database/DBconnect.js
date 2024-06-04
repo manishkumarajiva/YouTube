94111a7e489f80a67b8a9119772f3050bc4aa33a
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-04 23:49:13 +0530

    database setup

--- src/database/DBconnect.js
+++ src/database/DBconnect.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+
+const databaseConnection = (async (MONGODB_CONNECTION_STRING) => {
+    try {
+    
+    } catch (error) {
+        console.error(`MONGODB ERROR :: ${error}`)
+    }
+})(process.env.MONGODB_URL)
+
+
