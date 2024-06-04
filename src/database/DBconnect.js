4eefe229419e5b29ba224c0f31ca7c4c6b2fc933
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-04 23:54:29 +0530

    db connection

--- src/database/DBconnect.js
+++ src/database/DBconnect.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
+import ErrorHandler from "../utils/errorHandler.js";
 
 
 const databaseConnection = (async (MONGODB_CONNECTION_STRING) => {
     try {
-    
+        const connectionInstance = await mongoose.connect(MONGODB_CONNECTION_STRING);
+        if(!connectionInstance){
+            throw new ErrorHandler(500,`MONGODB CONNECTION ERROR :: ${connectionInstance}`)
+        }
+        console.log(`DATABASE CONNECTED SUCCESS || ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error(`MONGODB ERROR :: ${error}`)
     }
 })(process.env.MONGODB_URL)
 
-
+export default databaseConnection;
