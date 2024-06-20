5aa620a1d02eb076cba1c6d47edd00c4eed2675b
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-20 22:53:55 +0530

    razorpay config

---
+++ src/config/razorpay.config.js
@@ -0,0 +1,9 @@
+// import Razorpay from "razorpay";
+
+
+// var instanceRazarpay = new Razorpay({
+//   key_id: 'YOUR_KEY_ID',
+//   key_secret: 'YOUR_KEY_SECRET',
+// });
+
+// export default instanceRazarpay;
