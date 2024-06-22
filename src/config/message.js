db332d42bdc2447e11e456600f802f7bcf5b8dd8
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-22 21:53:35 +0530

    message

---
+++ src/config/message.js
@@ -0,0 +1,52 @@
+let msgCnfg = {
+    //LOGIN & LOGOUT
+    slogin: "Login Success 🫂",
+    flogin: "Login Failed ⛔",
+    slogout: "logout Success 🫂",
+    flogout : "logout failed ⛔",
+    notExist : "Not Exist 🚷",
+    notFound : "Not found 🪹 ",
+    session : "Session has been expired ⛔, Please Login again",
+
+    //REGISTERATION
+    sregister: "Successfully Registered 👦",
+    fregister: "Registeration Failed 🙆",
+
+    //PASSWORD
+    psupdate : "Password has been updated successfully 👍 ",
+    psfmatch : "Password is incorrect 🔴 ",
+
+
+    //PAYLOAD @EMAIL #PASSWORD
+    payload: "Invalid Payload Values 📛",
+    email: "Email already exist 📧",
+    sendEmail : "Email has been send successfully, Please check your email 📧 ",
+
+
+    //TOKEN
+    stoken: "User Login (Token 🎫 ) for Authentication ",
+    ftoken: "Token not Generated",
+    etoken : "Token 🎟️ has been expired, please login again",
+    elink : "This link has been expired 🙆 ",
+
+    //ROLE WISE AUTH
+    role : "Un-verified User Role 👦",
+
+    //CRUD OPERATION
+    screate: "Successfully Created 👍",
+    fcreate: "Not Created 🚫",
+    sdelete: "Successfully Deleted 👍",
+    fdelete: "Not Deleted 🚫",
+    supdate: "Successfully Updated 👍",
+    fupdate: "Not Updated 🚫",
+    exist: "Already Exist❗",
+    sread: "Successfully Fetch 🗒️",
+    fread: "Empty record ❕",
+    image: "Please Select Image 🟥",
+    imageType: "Image Type Should be jpg/jpeg 🔳",
+
+    //ERROR IN CATCHE
+    error: "Error occur in catch(err) 🥵"
+};
+
+module.exports =  msgCnfg;
