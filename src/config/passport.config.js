c1266917e20d430e953c56d743cae1ee93f8f0f7
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-25 23:01:38 +0530

    logger

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,29 +1,29 @@
-const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const UserModel = require('../models/user.model.js');
+import { use, serializeUser, deserializeUser } from "passport";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { findOrCreate, findById } from '../models/user.model.js';
 
 
 
-passport.use(new GoogleStrategy({
+use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://www.example.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
+    findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
 
 
-passport.serializeUser(function(user, done){
+serializeUser(function(user, done){
   done(null, user.id);
 });
 
 
-passport.deserializeUser(function(id, done){
-  UserModel.findById({ _id : id }).then((user) => {
+deserializeUser(function(id, done){
+  findById({ _id : id }).then((user) => {
     done(null, user);
   })
 });
