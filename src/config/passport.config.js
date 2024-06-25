10d90a922d5d8b4a7b1e8fd5de92be258752661b
manishkumarajiva <manishkumarajiva@gmail.com>
2024-06-25 22:57:34 +0530

    cookie parser

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,15 +1,29 @@
-// passport js
+const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const UserModel = require('../models/user.model.js');
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: ""
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
+
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+
+passport.deserializeUser(function(id, done){
+  UserModel.findById({ _id : id }).then((user) => {
+    done(null, user);
+  })
+});
