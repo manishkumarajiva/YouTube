6909bec39ceb144a86f79f1b1c7bdb3cae3b930b
manishkumarajiva <manishkumarajiva@gmail.com>
2024-07-02 00:02:49 +0530

    resours

--- SOURCE/utils/cloudinary.util.js
+++ SOURCE/utils/cloudinary.util.js
@@ -1,8 +1,21 @@
 import cloudinary from "../config/cloudnary.config";
 import fs from "fs";
+import ErrorHandler from "./errorHandler";
 
 
-const UploadMediaService = (mediafiles) => {
+const UploadMediaService = async (mediafiles) => {
+
+    try {
+        const filePath = mediafiles.path;
+        const options = {
+    
+        }
+    
+        const uploadResponse = await cloudinary.uploader.upload(filePath, options);
+        if
+    } catch (error) {
+        res.status(500).json(new ErrorHandler(500, "🔴 Cloudinary Error"))
+    }
 
 }
 
