f61814042afd102437ffee0a888c14c6bf5f6163
manishkumarajiva <manishkumarajiva@gmail.com>
2024-07-29 22:48:25 +0530

    remove folder

--- SOURCE/config/razorpay.config.js
+++
@@ -1,9 +0,0 @@
-// import Razorpay from "razorpay";
-
-
-// var instanceRazarpay = new Razorpay({
-//   key_id: 'YOUR_KEY_ID',
-//   key_secret: 'YOUR_KEY_SECRET',
-// });
-
-// export default instanceRazarpay;
--- SOURCE/routes/API/STUDIO/index.route.js
+++ SOURCE/routes/API/STUDIO/index.route.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 const router = Router();
 
-
 import VideoRoutes from "./video.route.js";
 
 
